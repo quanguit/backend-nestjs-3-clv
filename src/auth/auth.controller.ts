af134d2b4105e5edad22cd3965d4521fd9e096d1
QuangDo <quang.dn.clv@gmail.com>
2024-07-30 15:37:08 +0700

    add logout api and set some properties for cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { RefreshTokenDto } from './dtos/refresh-token.dto';
@@ -8,12 +10,35 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login(@Body() body: LoginDto) {
-    return this.authService.login(body);
+  async login(
+    @Body() body: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { access_token, refresh_token, expiredIn } =
+      await this.authService.login(body);
+
+    // Set the refresh token in a cookie
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      secure: true,
+      path: '/api/v1',
+    });
+
+    return {
+      access_token,
+      refresh_token,
+      expiredIn,
+    };
   }
 
   @Post('refresh-token')
   refreshToken(@Body() body: RefreshTokenDto) {
     return this.authService.refreshToken(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('logout')
+  logout(@Req() req: Request) {
+    return this.authService.logout(req.headers.authorization);
   }
 }
--- src/users/entities/session.entity.ts
+++ src/users/entities/session.entity.ts
@@ -15,6 +15,9 @@ export class SessionEntity {
   @Column()
   hash: string;
 
+  @Column({ type: 'boolean', default: false })
+  is_logout: boolean;
+
   @ManyToOne(() => UserEntity, (user) => user.sessions)
   @JoinColumn({ name: 'user_id' })
   user: UserEntity;
