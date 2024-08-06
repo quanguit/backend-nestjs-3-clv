28bd16562b142014fdad69c6d55e9344321f57a4
QuangDo <quang.dn.clv@gmail.com>
2024-08-06 16:27:23 +0700

    throw error when not init env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { LoggerModule } from './logger/logger.module';
+import { validate } from './shared/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate: validate,
     }),
     DatabaseModule,
     AuthModule,
---
+++ src/shared/config/env.validation.ts
@@ -0,0 +1,47 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, IsString, validateSync } from 'class-validator';
+
+export class EnvironmentVariables {
+  @IsNumber()
+  PORT: number;
+
+  @IsString()
+  DATABASE_HOST: string;
+
+  @IsNumber()
+  DATABASE_PORT: number;
+
+  @IsString()
+  DATABASE_USERNAME: string;
+
+  @IsString()
+  DATABASE_PASSWORD: string;
+
+  @IsString()
+  DATABASE_NAME: string;
+
+  @IsString()
+  SECRET: string;
+
+  @IsString()
+  EXP_IN_ACCESS_TOKEN: string;
+
+  @IsString()
+  EXP_IN_REFRESH_TOKEN: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
